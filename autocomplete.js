2e0b2a799e288a8a582f3026b2ce9e6bfb19d946
Marcelo A Naegeler <marcelo.andre.naegeler@gmail.com>
2016-02-10 17:41:36 -0200

    Resolved bugs and added funcionality

--- autocomplete.js
+++ autocomplete.js
@@ -1,7 +1,9 @@
-;( function () {
+;var Autocomplete = ( function () {
 	"use strict";
 
-	var notSearchKeys = [ 16, 17, 18, 37, 38, 39, 40 ];
+	var notSearchKeys = [ 13, 16, 17, 18, 37, 38, 39, 40 ];
+	var optionTemplate = document.getElementById( 'tpl-option' ).innerHTML;
+	var resultList;
 
 	if ( !window.ajax ) {
 		var ajax = ( function () {
@@ -28,36 +30,171 @@
 		})();
 	}
 
-	var autocomplete = function ( url ) {
+
+	/*
+	* Actions to change on autocomplete box
+	**/
+	var actions = ( function () {
 		
+		var down = function () {
+			var items = this.childNodes;
+			for ( var i = 0, l = items.length; i < l; i++ ) {
+				var el = items[ i ];
+				if ( el.classList && el.classList.contains( 'selected' ) ) {
+					el.classList.remove( 'selected' );
+
+					if ( el.nextElementSibling ) {
+						el.nextElementSibling.classList.add( 'selected' );
+					} else {
+						el.parentNode.firstChild.classList.add( 'selected' );
+					}
+					break; // Break loop to avoid removing the class
+				}
+			}
+		};
+
+		var up = function () {
+			var items = this.childNodes;
+			for ( var i = 0, l = items.length; i < l; i++ ) {
+				var el = items[ i ];
+				if ( el.classList && el.classList.contains( 'selected' ) ) {
+					el.classList.remove( 'selected' );
+					
+					if ( el.previousElementSibling ) {
+						el.previousElementSibling.classList.add( 'selected' );
+					} else {
+						el.parentNode.lastChild.classList.add( 'selected' );
+					}
+					break; // Break loop to avoid removing the class
+				}
+			}
+		};
+
+		var select = function ( isClick, callback, input ) {
+			
+			if ( isClick ) {
+				
+				var sel = { id: this.dataset.id, label: this.innerText };
+				callback && callback.call( sel );
+				boxDisplay.hide.call( input );
+
+			} else {
+
+				var items = this.childNodes;
+				for ( var i = 0, l = items.length; i < l; i++ ) {
+					var el = items[ i ];
+					if ( el.classList && el.classList.contains( 'selected' ) ) {
+
+						var sel = { id: el.dataset.id, label: el.innerText };
+						callback && callback.call( sel );
+						boxDisplay.hide.call( input );
+
+						break;
+					}
+				}
+				
+			}
+		};
+
+		return {
+			down: down
+			, up: up
+			, select: select
+		};
+
+	})();
+
+
+	var boxDisplay = ( function () {
+
+		var show = function ( a ) {
+			// ~a~ is a data array
+			// ~this~ refers to the input
+			var t = templateEngine( optionTemplate, { data: a }, true );
+			var children = this.parentNode.childNodes;
+
+			for ( var i = 0, l = children.length; i < l; i++ ) {
+				var c = children[ i ];
+				if ( c.classList && c.classList.contains( 'autocomplete' ) ) {
+					c.innerHTML = t;
+					resultList = c.childNodes[ 0 ]; // Get the first element "ul"
+				}
+			}
+		};
+
+		var hide = function () {
+			// ~this~ refers the input
+			var children = this.parentNode.childNodes;
+			for ( var i = 0, l = children.length; i < l; i++ ) {
+				var c = children[ i ];
+				if ( c.classList && c.classList.contains( 'autocomplete' ) ) {
+					c.innerHTML = '';
+				}
+			}
+		};
+
+		return {
+			show: show
+			, hide: hide
+		};
+	})();
+
+
+	var init = function ( opt ) {
+		
+		var input = this;
+
 		var request = function ( d, cb ) {
 			ajax.get({
-				url: url
+				url: opt.source
 				, data: '?term='+ d
 				, success: cb
 			});
 		};
 
-		var displayOptions = function ( a ) {
-			// a is an array
-			console.log( a );
-		};
-
-		this.onkeyup = function ( event ) {
-
-			if ( notSearchKeys.indexOf( event.keyCode ) > -1 ) {
+		var doRequest = function () {
+			if ( !input.value ) {
 				return;
 			}
-
-			request( this.value, function ( d ) {
+			request( input.value, function ( d ) {
 				d = JSON.parse( d );
-				displayOptions( d );
+
+				boxDisplay.show.call( input, d );
 			});
 		};
 
-		
+		this.onblur = function () {
+			boxDisplay.hide.call( this );
+		};
+
+		this.onfocus = doRequest;
+
+		this.onkeydown = function ( event ) {
+			var k = event.keyCode;
+			if ( notSearchKeys.indexOf( k ) > -1 ) {
+				return false;
+			}
+		};
+
+		this.onkeyup = function ( event ) {
+			var key = event.keyCode;
+			if ( notSearchKeys.indexOf( key ) > -1 ) {
+				if ( !!this.value && ( key === 13 || key === 38 || key === 40 ) ) {
+					key === 38 && actions.up.call( resultList );
+					key === 40 && actions.down.call( resultList );
+					key === 13 && actions.select.call( resultList, null, opt.select, input );
+				}
+				return;
+			}
+
+			doRequest();
+		};
+
 	};
 
+	return {
+		init: init
+		, actions: actions
+	};
 
-	window.Autocomplete = autocomplete;
 })();
